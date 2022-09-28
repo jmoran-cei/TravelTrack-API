c6118a95815252c53a21a26ea0caf717b2cc5425
Jon Moran <jmoran@ceiamerica.com>
2022-09-28 11:34:10 -0400

    Generate swagger documentation

--- Program.cs
+++ Program.cs
@@ -1,11 +1,42 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200");
+            policy.WithMethods("GET", "POST", "OPTIONS", "PUT", "DELETE");
+            policy.WithHeaders("Content-Type");
+        });
+});
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "TravelTrack API",
+            Description = "An ASP.NET Core Web API for TravelTrack",
+            Contact = new OpenApiContact
+            {
+                Name = "Jon Moran",
+                Email = "jmoran@ceiamerica.com",
+                Url = new Uri("https://www.ceiamerica.com/")
+            },
+            Version = "v1"
+        });
+        c.EnableAnnotations();
+        c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, 
+            $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"));
+    });
 
 var app = builder.Build();
 
@@ -15,6 +46,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TravelTrack v1"));
 }
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
